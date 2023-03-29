6945125e129aed304545aa53029f6ee9549a849e
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-03-29 16:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function threeSum(arr, target) {
 // write your code here
+	var n = arr.length;
+	for(var i=0;i<n;i++){
+		for(var j=i+1;j<n;j++){
+			for(var k=j+1;k<n;k++){
+				if((arr[i]+arr[j]+arr[k])==target){
+					return target;
+				}
+			}
+		}
+		return 0;
+	}
   
 }
 
